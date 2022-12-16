2b7ee0363841e3a935dc09268af590d8238c6e95
Iampooo <70931477+Iampooo@users.noreply.github.com>
2022-12-16 10:46:41 +0800

    Add files via upload

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,73 +1,104 @@
-import React from 'react'
-import { Flex, Box, Text, Button, Input, useToast, Spacer } from '@chakra-ui/react'
-import { useState } from 'react'
-import { ethers } from 'ethers'
-import kryptoCampNFTAbi from './KryptoCampNft.json'
-import { useEffect } from 'react'
+import React from "react";
+import {
+  Flex,
+  Box,
+  Text,
+  Button,
+  Input,
+  useToast,
+  Spacer,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { ethers } from "ethers";
+import kryptoCampNFTAbi from "./KryptoCampNft.json";
+import { useEffect } from "react";
 
 const KryptoCampNFTAddress = "0x5A5E65f915b8bCC6d856FEF6ed81f895062882d1";
 
 const MainMint = ({ accounts, setAccounts }) => {
-  const [mintAmount, setMintAmount] = useState(1)
-  const [totalSupply, setTotalSupply] = useState(0)
-  const isConnected = Boolean(accounts[0])
-  const toast = useToast()
+  const [mintAmount, setMintAmount] = useState(1);
+  const [totalSupply, setTotalSupply] = useState(0);
+  const isConnected = Boolean(accounts[0]);
+  const toast = useToast();
 
   // TODO: 呼叫合約 totalSupply 方法，並寫入到變數 totalSupply
   const getNFTTotalSupply = async () => {
-
-  }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const KryptoCampContract = new ethers.Contract(
+      KryptoCampNFTAddress,
+      kryptoCampNFTAbi,
+      provider
+    );
+    const currentSupply = await KryptoCampContract.totalSupply();
+    const amount = ethers.utils.formatUnits(currentSupply, 0);
+    setTotalSupply(amount);
+  };
 
   // TODO: 呼叫 Contract mint fn
   const handleMint = async () => {
     if (window.ethereum) {
       // TODO: 1) 設定 Provider
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
 
       // TODO: 2) 設定 signer
-
+      const signer = provider.getSigner();
       // TODO: 3) new Contract 實體
-
+      const KryptoCampContract = new ethers.Contract(
+        KryptoCampNFTAddress,
+        kryptoCampNFTAbi,
+        signer
+      );
 
       try {
         // TODO: 4) 呼叫合約 mint 方法
-
+        console.log(mintAmount);
+        const mintPrice = 0.01;
+        const response = await KryptoCampContract.mint(mintAmout, {
+          value: ethers.utils.parseEther(
+            (0.01 * mintAmount).toString(),
+            "ether"
+          ),
+        });
+        console.log("response", response);
       } catch ({ error }) {
-        showToast(error.message)
-        console.error('[Error]', error)
+        showToast(error.message);
+        console.error("[Error]", error);
       }
     }
-  }
+  };
 
   const handleDecrement = () => {
-    if (mintAmount <= 1) return
+    if (mintAmount <= 1) return;
 
-    setMintAmount(mintAmount - 1)
-  }
+    setMintAmount(mintAmount - 1);
+  };
 
   const handleIncrement = () => {
-    if (mintAmount >= 3) return
-    setMintAmount(mintAmount + 1)
-  }
+    if (mintAmount >= 3) return;
+    setMintAmount(mintAmount + 1);
+  };
 
   // 顯示錯誤訊息
   const showToast = (error) => {
     toast({
       title: `發生錯誤：${error}`,
-      status: 'error',
-      position: 'top',
+      status: "error",
+      position: "top",
       isClosable: true,
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-    getNFTTotalSupply()
-  }, [])
+    getNFTTotalSupply();
+  }, []);
 
   return (
     <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
       <Box width="520px">
         <div className="mint-container">
-          <Text fontSize="48px" textShadow="0 5px #000">KryptoCamp</Text>
+          <Text fontSize="48px" textShadow="0 5px #000">
+            KryptoCamp
+          </Text>
           <Text
             fontSize="30px"
             letterSpacing="0.5%"
@@ -75,12 +106,10 @@ const MainMint = ({ accounts, setAccounts }) => {
             textShadow="0 2px 2px #000"
             lineHeight={"26px"}
           >
-            It's 2043.
-            Can the KryptoCamp save humans from destructive rampant NFT speculation? Mint KryptoCamp to find out!
+            It's 2043. Can the KryptoCamp save humans from destructive rampant
+            NFT speculation? Mint KryptoCamp to find out!
           </Text>
           <Spacer />
-
-
         </div>
 
         {isConnected ? (
@@ -164,7 +193,7 @@ const MainMint = ({ accounts, setAccounts }) => {
         )}
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default MainMint
+export default MainMint;
